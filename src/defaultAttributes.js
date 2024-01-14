e5270b2842ee4f23c5b8b1e638f3b9d83e583421
dntzhang <dntzhang@qq.com>
2024-01-14 08:55:55 +0800

    fix: strokeWidth & strokeLinecap & strokeLinejoin

--- src/defaultAttributes.js
+++ src/defaultAttributes.js
@@ -12,9 +12,9 @@ var defaultAttributes = {
   viewBox: "0 0 24 24",
   fill: "none",
   stroke: "currentColor",
-  strokeWidth: 2,
-  strokeLinecap: "round",
-  strokeLinejoin: "round"
+  'stroke-width': 2,
+  'stroke-linecap': "round",
+  'stroke-linejoin': "round"
 };
 
 export { defaultAttributes as default };
